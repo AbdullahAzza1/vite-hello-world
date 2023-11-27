8eeca45b67745d7f18dfccc888a823227be37e56
AbdullahAzza1 <abdullahazza141@gmail.com>
2023-11-27 18:10:26 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import AppTitle  from './components/AppTitle.vue'
+import AppHero from './components/AppHero.vue'
+
+export default {
+  components: {
+    AppTitle,
+    AppHero
+  }
+}
+</script>
+
+<template>
+  <div>
+    Benvenuto nella tua App in Vue.js
+  </div>
+  <AppTitle />
+  <AppHero />
+</template>
+
+<style scoped></style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,8 @@
+<script>
+</script>
+
+<template>
+  <h1>la mia prima volta con vite</h1>
+</template>
+
+<style scoped></style>
