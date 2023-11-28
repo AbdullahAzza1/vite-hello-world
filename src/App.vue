942e6afa473f9213b045ba45c029605725f4842e
AbdullahAzza1 <abdullahazza141@gmail.com>
2023-11-28 16:02:33 +0100

    step3

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
 <script>
-import AppTitle  from './components/AppTitle.vue'
+import AppTitle from './components/AppTitle.vue'
 
 
 export default {
-  components: {
-    AppTitle 
-  }
+	components: {
+		AppTitle
+	}
 }
 </script>
 
 <template>
-  <div>
-    Benvenuto nella tua App in Vue.js
-  </div>
-  <AppTitle />
+	<AppTitle />
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, qui dolores. Iusto asperiores itaque explicabo
+		ducimus quia qui reprehenderit eius consequatur natus magni numquam in, facilis culpa odit! Inventore, et.
+		Enim velit vero perferendis aliquam odit minus cumque facilis illum vitae non deleniti, accusamus earum iste
+		distinctio eaque quis pariatur! Non necessitatibus corporis beatae rerum a quae, sed ea vitae.
+		Optio nemo blanditiis nihil, ullam nam quos. Et asperiores doloremque dicta optio, natus, blanditiis quidem corporis
+		maxime repellendus error, ratione voluptate illo perspiciatis nulla explicabo suscipit est consequuntur iusto odio!
+		Quaerat, explicabo modi hic suscipit excepturi distinctio vitae sit quibusdam, vero est assumenda ducimus itaque
+		fuga aut quas! Earum ducimus nobis ipsa consectetur rem fuga error tempora exercitationem alias iste.</p>
 </template>
 
 <style scoped></style>
--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -2,10 +2,8 @@
 </script>
 
 <template>
-  <h1>la mia prima volta con vite</h1>
-  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem consectetur velit neque. Voluptatum, dolorem molestias, totam architecto velit similique quod odit quia quas neque accusamus eius culpa, praesentium ipsa. Expedita?
-  Temporibus ex, praesentium voluptates libero provident eos quam a tempora explicabo, recusandae, dignissimos cupiditate. Deserunt doloribus quae eligendi dolores blanditiis perferendis laboriosam dolor tenetur? Nemo cupiditate nihil adipisci sapiente sunt.
-  Facere praesentium corrupti asperiores tempora. Laboriosam vitae, a qui illum at culpa quibusdam tempora rerum veritatis sit dolorem sequi similique magni laudantium obcaecati, alias illo officiis facere. Suscipit, consequuntur illum!</p>
+	<h1>la mia prima volta con vite</h1>
+	
 </template>
 
 <style scoped></style>
